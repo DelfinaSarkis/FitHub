91f2fa80e52ffdd7321dc90b9704d0866f667b7f
Evaristo <114101006+EvaristoAguirre@users.noreply.github.com>
2024-07-26 16:40:38 -0300

    guardado antes de integracion prueba

--- src/PlanDeEntranmiento/Plan.repository.ts
+++ src/PlanDeEntranmiento/Plan.repository.ts
@@ -1,68 +1,95 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Plan } from "./Plan.entity";
-import { Check, ILike, Repository } from "typeorm";
-import { Category } from "src/Category/Category.entity";
-import { DifficultyLevel } from "./difficultyLevel.enum";
-import { PlanCreateDto } from "./CreatePlan.dto";
-import { Users } from "src/User/User.entity";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Plan } from './Plan.entity';
+import { Check, ILike, Repository } from 'typeorm';
+import { Category } from 'src/Category/Category.entity';
+import { DifficultyLevel } from './difficultyLevel.enum';
+import { PlanCreateDto } from './CreatePlan.dto';
+import { Users } from 'src/User/User.entity';
 
 @Injectable()
 export class PlanRepository {
-    constructor(@InjectRepository(Plan) private planRepository:Repository<Plan>, @InjectRepository(Users) private userRepository:Repository<Users>){}
+  constructor(
+    @InjectRepository(Plan) private planRepository: Repository<Plan>,
+    @InjectRepository(Users) private userRepository: Repository<Users>,
+  ) {}
 
-    async getPlan(page: number, limit: number,category?:string,location?:string,difficultyLevel?:DifficultyLevel,search?:string) {
-        
-        let whereConditions: any = { isActive: true, check: true };
-        if (category !== undefined) {
-            whereConditions.category = category;
-        }
-        
-        if (location !== undefined) {
-            whereConditions.location = location;
-        }
-
-        if (difficultyLevel !== undefined) {
-            whereConditions.difficultyLevel = difficultyLevel;
-        }
-        if (search !== undefined) {
-            const stopWords = new Set(['de', 'y', 'el', 'la', 'en', 'a', 'o']); // Lista de palabras de parada
-            const arrSearch = search.split(' ').filter(term => term.trim() !== '' && !stopWords.has(term.toLowerCase()));
-        
-            whereConditions = arrSearch.map(term => ({...whereConditions,name:(ILike(`%${term}%`))}));
-        }
-        return this.planRepository.find({ 
-            where: whereConditions,
-            skip: (page - 1) * limit,
-            take: limit
-        });
+  async getPlan(
+    page: number,
+    limit: number,
+    category?: string,
+    location?: string,
+    difficultyLevel?: DifficultyLevel,
+    search?: string,
+  ) {
+    let whereConditions: any = { isActive: true, check: true };
+    if (category !== undefined) {
+      whereConditions.category = category;
     }
 
-    async getPlanById(id){
-        return await this.planRepository.findOne( { where: {id, isActive:true}, } );
+    if (location !== undefined) {
+      whereConditions.location = location;
     }
 
-    async createPlan(plan: PlanCreateDto, admin:string) {
-        const adm = await this.userRepository.findOne({ where: { id: admin } });
-        if (!adm) { throw new NotFoundException('Usuario no encontrado')};
-        const planCreado = await this.planRepository.create(plan);
-        planCreado.admin = adm;
-        await this.planRepository.save(planCreado);
-        return 'Plan creado';
+    if (difficultyLevel !== undefined) {
+      whereConditions.difficultyLevel = difficultyLevel;
     }
+    if (search !== undefined) {
+      const stopWords = new Set(['de', 'y', 'el', 'la', 'en', 'a', 'o']); // Lista de palabras de parada
+      const arrSearch = search
+        .split(' ')
+        .filter(
+          (term) => term.trim() !== '' && !stopWords.has(term.toLowerCase()),
+        );
 
-    async updatePlan(plan, identificacion, admin){
-        const userAdmin = await this.userRepository.findOne({ where: { id: admin } });
-        const planToUpdate = await this.planRepository.findOne({ where: { id:identificacion, admin:userAdmin} });
-        if (!planToUpdate || planToUpdate.isActive === false) { throw new NotFoundException('Plan no encontrado o eliminado')};
-        return await this.planRepository.update(identificacion, plan);
+      whereConditions = arrSearch.map((term) => ({
+        ...whereConditions,
+        name: ILike(`%${term}%`),
+      }));
     }
+    return this.planRepository.find({
+      where: whereConditions,
+      skip: (page - 1) * limit,
+      take: limit,
+    });
+  }
 
-    async deletePlan(id){
-        const plan = await this.planRepository.findOne({ where: { id: id } });
-        if (!plan || plan.isActive === false) { throw new NotFoundException('Plan no encontrado o eliminado')};
-        await this.planRepository.update( id, {...plan, isActive: false});
+  async getPlanById(id) {
+    return await this.planRepository.findOne({ where: { id, isActive: true } });
+  }
 
-        return id;
+  async createPlan(plan: PlanCreateDto, admin: string) {
+    const adm = await this.userRepository.findOne({ where: { id: admin } });
+    if (!adm) {
+      throw new NotFoundException('Usuario no encontrado');
     }
+    const planCreado = await this.planRepository.create(plan);
+    planCreado.admin = adm;
+    await this.planRepository.save(planCreado);
+    return 'Plan creado';
+  }
+
+  async updatePlan(plan, identificacion, admin) {
+    const userAdmin = await this.userRepository.findOne({
+      where: { id: admin },
+    });
+    const planToUpdate = await this.planRepository.findOne({
+      where: { id: identificacion, admin: userAdmin },
+    });
+    if (!planToUpdate || planToUpdate.isActive === false) {
+      throw new NotFoundException('Plan no encontrado o eliminado');
+    }
+    return await this.planRepository.update(identificacion, plan);
+  }
+
+  async deletePlan(id) {
+    const plan = await this.planRepository.findOne({ where: { id: id } });
+    if (!plan || plan.isActive === false) {
+      throw new NotFoundException('Plan no encontrado o eliminado');
+    }
+    await this.planRepository.update(id, { ...plan, isActive: false });
+
+    return id;
+  }
 }
--- src/PlanDeEntranmiento/Plan.service.ts
+++ src/PlanDeEntranmiento/Plan.service.ts
@@ -1,30 +1,45 @@
-import { Category } from "src/Category/Category.entity";
-import { PlanRepository } from "./Plan.repository";
-import { DifficultyLevel } from "./difficultyLevel.enum";
-import { Inject, Injectable } from "@nestjs/common";
-import { PlanCreateDto, PlanUpdateDto } from "./CreatePlan.dto";
-import { log } from "console";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Category } from 'src/Category/Category.entity';
+import { PlanRepository } from './Plan.repository';
+import { DifficultyLevel } from './difficultyLevel.enum';
+import { Inject, Injectable } from '@nestjs/common';
+import { PlanCreateDto, PlanUpdateDto } from './CreatePlan.dto';
+import { log } from 'console';
 
 @Injectable()
 export class PlanService {
-    constructor(private readonly planRepository:PlanRepository){}
-    async getPlan(page: string, limit: string,category?:string,location?:string,difficultyLevel?:DifficultyLevel,search?:string){
-        return this.planRepository.getPlan(Number(page), Number(limit),category, location, difficultyLevel,search);
-    }
+  constructor(private readonly planRepository: PlanRepository) {}
+  async getPlan(
+    page: string,
+    limit: string,
+    category?: string,
+    location?: string,
+    difficultyLevel?: DifficultyLevel,
+    search?: string,
+  ) {
+    return this.planRepository.getPlan(
+      Number(page),
+      Number(limit),
+      category,
+      location,
+      difficultyLevel,
+      search,
+    );
+  }
 
-    async getPlanById(id){
-        return await this.planRepository.getPlanById(id);
-    }
+  async getPlanById(id) {
+    return await this.planRepository.getPlanById(id);
+  }
 
-    async createPlan(plan:PlanCreateDto, admin:string){
-        return await this.planRepository.createPlan(plan,admin);
-    }
+  async createPlan(plan: PlanCreateDto, admin: string) {
+    return await this.planRepository.createPlan(plan, admin);
+  }
 
-    async updatePlan(plan:PlanUpdateDto, identificacion:string, admin:string){
-        await this.planRepository.updatePlan(plan, admin, identificacion);
-    }
+  async updatePlan(plan: PlanUpdateDto, identificacion: string, admin: string) {
+    await this.planRepository.updatePlan(plan, admin, identificacion);
+  }
 
-    async deletePlan(id){
-        return await this.planRepository.deletePlan(id);
-    }
+  async deletePlan(id) {
+    return await this.planRepository.deletePlan(id);
+  }
 }
